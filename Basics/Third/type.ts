c8e859a519ff619247754b31139b143ec0589652
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-03 19:21:10 -0400

    🔨 implement typescript tuples, boolean, and object.

--- Basics/Third/type.ts
+++ Basics/Third/type.ts
@@ -3,9 +3,28 @@
 
 // TS string literal
 let str: string = 'string';
+
 // TS number
 let num: number = 5;
+
 // TS array
 let arr: Array<number> = [1, 2, 3];
 let heroes: Array<string> = ['Wanda', 'Thor', 'Hulk'];
 let items: number[] = [1, 2, 3];
+
+// TS tuples
+let address: [string, number] = ['ginger', 100];
+
+// TS object
+let obj: object = {};
+let dog: object = {
+    name: 'Happy',
+    age: 3
+};
+let person: { name: string, age: number } = {
+    name: 'Harry',
+    age: 25
+};
+
+// TS boolean
+let isTrue: boolean = true;
