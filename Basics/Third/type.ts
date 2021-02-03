aa0472a3c88243bd405dc6fecb5f72dbc5da8b3c
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-03 19:00:50 -0400

    🔧 implement basic typescript string, number, array.

---
+++ Basics/Third/type.ts
@@ -0,0 +1,11 @@
+// JS string literal
+// let str = 'string';
+
+// TS string literal
+let str: string = 'string';
+// TS number
+let num: number = 5;
+// TS array
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Wanda', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
