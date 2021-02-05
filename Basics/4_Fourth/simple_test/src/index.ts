c8a23d11d48c369cd58a22fe5cbc2109f9e3c396
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-04 21:17:13 -0400

    🔨 start typing to variables and methods in index.ts. Also turn noimplcitAny on.

--- Basics/4_Fourth/simple_test/src/index.ts
+++ Basics/4_Fourth/simple_test/src/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
-let todoItems;
+let todoItems: object[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): object[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -12,30 +12,30 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: object): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: object): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): boolean {
   return todoItems.filter(item => item.done);
 }
 
@@ -45,7 +45,7 @@ function addTwoTodoItems() {
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
