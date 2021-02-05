7f7f5cf67b6a4e15456810441bb904a97caf3a07
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-05 18:53:03 -0400

    🔨 implement dictionary pattern

--- Basics/4_Fourth/interface/interface.ts
+++ Basics/4_Fourth/interface/interface.ts
@@ -30,3 +30,27 @@ let a: SumFunction;
 a = function (a: number, b: number) {
     return a + b;
 };
+
+// inteface having index
+interface StringArray {
+    [index: number]: string
+}
+
+let strArr: StringArray = ['a', 'b', 'c'];
+strArr[0] = 'd';
+
+// Dictionary pattern
+interface StringRegexDictionary {
+    [key: string]: RegExp
+}
+
+let fileRegex: StringRegexDictionary = {
+    'cssFile': /\.css$/,
+    'htmlFile': /\.html$/
+};
+
+// fileRegex['cssFile'] = 'a';
+
+Object.keys(fileRegex).forEach(value => { });
+
+
