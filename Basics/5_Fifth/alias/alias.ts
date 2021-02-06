8ca8e4f46d496c0b7d683ea610be6f84d473b81d
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-05 20:16:36 -0400

    🔨 use alias

---
+++ Basics/5_Fifth/alias/alias.ts
@@ -0,0 +1,13 @@
+type MyString = string;
+
+let yString: MyString = 'hi';
+
+type Book = {
+    name: string;
+    genre: string;
+};
+
+let harry: Book = {
+    name: 'Harry',
+    genre: 'Fantage'
+};
