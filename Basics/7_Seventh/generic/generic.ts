d6417486226ad9f08c9de7aa79cf678d77b30b22
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-06 22:29:42 -0400

    🔨 difference between union and generic

--- Basics/7_Seventh/generic/generic.ts
+++ Basics/7_Seventh/generic/generic.ts
@@ -1,7 +1,18 @@
 // Generic is a template of datatype, it is unknown type unless it is instantiated and decalres type.
-function smsText<T>(text: T): T {
+// function smsText<T>(text: T): T {
+//     console.log(text);
+//     return text;
+// }
+
+// smsText<string>('Hi~~~');
+
+// If you use union type, you can't use built-in only string or number method.
+// You can only use same methods in both string, number.
+// That's why we need to use Generic.
+function smsText(text: string | number) {
     console.log(text);
     return text;
 }
 
-smsText<string>('Hi~~~');
+let send = smsText('hi');
+send.split('').reverse.join();
