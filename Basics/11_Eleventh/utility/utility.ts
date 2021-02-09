8361dd0b0315e21f0227e8180791cd1b25abf5ff
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-09 10:05:52 -0400

    🔨 introduce omit and partial

--- Basics/11_Eleventh/utility/utility.ts
+++ Basics/11_Eleventh/utility/utility.ts
@@ -10,3 +10,19 @@ interface Product {
 function myShoppingItem(item: Pick<Product, 'id' | 'name' | 'price'>): void {
     //...
 }
+
+// Omit utility
+let myProduct: Omit<Product, 'checked' | 'quantity'> = {
+    id: 1,
+    name: 'food',
+    price: 500
+};
+
+
+// Partial utility => propoerties in interface became optional
+// 아래 타입을 옵셔널을 보기 쉽게 하기 위한 예
+type UpdateProduct = Partial<Product>;
+
+let updateProduct: Partial<Product> = {
+    quantity: 5,
+};
