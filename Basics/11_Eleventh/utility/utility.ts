6fab6c2c81af64cfe5094f4fd0b37ec4fe37e416
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-09 10:20:20 -0400

    🔨 analyze structure of partial utility

--- Basics/11_Eleventh/utility/utility.ts
+++ Basics/11_Eleventh/utility/utility.ts
@@ -26,3 +26,33 @@ type UpdateProduct = Partial<Product>;
 let updateProduct: Partial<Product> = {
     quantity: 5,
 };
+
+
+// How to create the structure of partial utility
+interface Software {
+    name: string;
+    description: string;
+    serialnumber: number;
+}
+
+// #1 how to update some properties in the software
+type UpdateSoftware1 = {
+    name?: Software['name'];
+    description?: Software['description'];
+    serialnumber?: Software['serialnumber'];
+}
+
+// #2
+type UpdateSoftware2 = {
+    [p in 'name' | 'description' | 'serialnumber']?: Software[p];
+}
+
+// #3
+type UpdateSoftware3 = {
+    [p in keyof Software]?: Software[p];
+}
+
+// #4 This is the structure of partial utility.
+type UpdateSoftware4<T> = {
+    [p in keyof T]?: T[p];
+}
